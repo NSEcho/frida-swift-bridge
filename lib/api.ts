e760b81e9d42dd4d3eb44c6c78f1eb4c2a724880
NSEcho <xdaemonx@protonmail.ch>
2023-05-23 20:29:08 +0200

    update

--- lib/api.ts
+++ lib/api.ts
@@ -58,7 +58,7 @@ export function getPrivateAPI(): API {
         return cachedPrivateAPI;
     }
 
-    var imports = Module._enumerateImports();
+    var imports = Module.enumerateImports();
     console.log(imports);
 
     Module.ensureInitialized("Foundation");
